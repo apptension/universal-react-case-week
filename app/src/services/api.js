6965580e0b755f8d9860e8e203b3514270e0f443
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-04-06 18:22:15 +0200

    Update api service

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -1,5 +1,6 @@
 import fetch from 'isomorphic-fetch';
 import md5 from 'md5';
+import url from 'url';
 
 class ApiService {
   constructor() {
@@ -8,28 +9,32 @@ class ApiService {
     this.marvelApiPrivateKey = '98f6eaf7cdf45f76b8f33251e4b57195c937a68e';
   }
 
+  buildUrl(resource) {
+    const timestamp = new Date().getTime();
+    const parsedURL = url.parse(this.marverlApiUrl + resource, true);
+    return url.format(Object.assign({}, parsedURL, {
+      query: Object.assign({}, parsedURL.query, {
+        apikey: this.marvelApiKey,
+        ts: timestamp,
+        hash: md5(timestamp + this.marvelApiPrivateKey + this.marvelApiKey)
+      }),
+      search: null
+    }));
+  }
+
   getCharacterList() {
-    let timestamp = new Date().getTime();
-    let hash = md5(timestamp + this.marvelApiPrivateKey + this.marvelApiKey);
-    return fetch(this.marverlApiUrl + 'characters?limit=100&apikey=' +
-      this.marvelApiKey + '&ts=' + timestamp + '&hash=' + hash)
-      .then((data) => {
-        return data.json();
-      })
+    const characterListUrl = this.buildUrl('characters?limit=100');
+    return fetch(characterListUrl)
+      .then((data) => data.json())
       .then((result) => result.data.results);
   }
 
   getCharacter(id) {
-    let timestamp = new Date().getTime();
-    let hash = md5(timestamp + this.marvelApiPrivateKey + this.marvelApiKey);
-    return fetch(this.marverlApiUrl + 'characters/' + id + '?apikey=' +
-      this.marvelApiKey + '&ts=' + timestamp + '&hash=' + hash)
-      .then((data) => {
-        return data.json();
-      })
+    const characterUrl = this.buildUrl(`characters/${id}`);
+    return fetch(characterUrl)
+      .then((data) => data.json())
       .then((result) => result.data.results[0]);
   }
 }
 
-let apiServiceInstance = new ApiService();
-export default apiServiceInstance;
+export default new ApiService();
