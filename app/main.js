c494abeefae916fd9fecbb87fd3bdf35c294072a
Patryk Ziemkowski <pziemkowski@apptension.com>
2016-04-06 16:40:17 +0200

    Iteration 3

--- app/main.js
+++ app/main.js
@@ -1,18 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-material-design/dist/bootstrap-material-design.min.css';
 import './main.scss';
+import 'images/favicon.png';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Transmit from "react-transmit";
-import * as ReactRouter from 'react-router';
+import {Router, browserHistory} from 'react-router';
+
 
 import routes from './routes.js';
-
-require('images/favicon.png');
 
 window.React = React;
 
 const reactRoot = window.document.getElementById('react-root');
 
-Transmit.render(ReactRouter.Router, {routes: routes, history: ReactRouter.browserHistory}, reactRoot);
+ReactDOM.render(<Router routes={routes} history={browserHistory}/>, reactRoot);
